62984da5c9dd849b5630ecc24e18731c394aca44
SpinnerX <heraaronhotmail@yahoo.com>
2025-04-02 20:22:18 -0700

    Updated to conform to clang-format

--- src/renderer/first_person_camera.cpp
+++ src/renderer/first_person_camera.cpp
@@ -1,33 +1,50 @@
 #include <renderer/first_person_camera.hpp>
 
-first_person_camera::first_person_camera(float p_aspect_ratio, const glm::vec3& p_pos, const glm::vec3& p_target, const glm::vec3& p_up, const camera_properties& p_properties) {
+first_person_camera::first_person_camera(
+  float p_aspect_ratio,
+  const glm::vec3& p_pos,
+  const glm::vec3& p_target,
+  const glm::vec3& p_up,
+  const camera_properties& p_properties) {
     m_position = p_pos;
     m_aspect_ratio = p_aspect_ratio;
 
-    if(camera_orientation_left_hand) {
+    if (camera_orientation_left_hand) {
         m_orientation = glm::lookAtLH(m_position, p_target, p_up);
-        m_projection = glm::perspectiveLH(p_properties.Fov, m_aspect_ratio, p_properties.ZNear, p_properties.ZFar);
+        m_projection = glm::perspectiveLH(p_properties.Fov,
+                                          m_aspect_ratio,
+                                          p_properties.ZNear,
+                                          p_properties.ZFar);
     }
     else {
         m_orientation = glm::lookAtRH(m_position, p_target, p_up);
-        m_projection = glm::perspectiveRH(p_properties.Fov, m_aspect_ratio, p_properties.ZNear, p_properties.ZFar);
+        m_projection = glm::perspectiveRH(p_properties.Fov,
+                                          m_aspect_ratio,
+                                          p_properties.ZNear,
+                                          p_properties.ZFar);
     }
 }
 
-glm::mat4 first_person_camera::projection() {
+glm::mat4
+first_person_camera::projection() {
     return glm::mat4(1.f);
 }
 
-glm::vec3 first_person_camera::position() { return m_position; }
+glm::vec3
+first_person_camera::position() {
+    return m_position;
+}
 
-glm::mat4 first_person_camera::view_mat() {
+glm::mat4
+first_person_camera::view_mat() {
     glm::mat4 t = glm::translate(glm::mat4(1.f), -m_position);
     glm::mat4 r = glm::mat4_cast(m_orientation);
     glm::mat4 view = r * t;
     return view;
 }
 
-glm::mat4 first_person_camera::view_proj_mat() {
+glm::mat4
+first_person_camera::view_proj_mat() {
     glm::mat4 view = view_mat();
     glm::mat4 vp = m_projection * view;
 
--- src/vulkan-cpp/vk_context.cpp
+++ src/vulkan-cpp/vk_context.cpp
@@ -4,15 +4,19 @@
 #include <GLFW/glfw3.h>
 #include <vulkan-cpp/logger.hpp>
 
-
 namespace vk {
     const std::vector<const char*> validation_layers = {
         "VK_LAYER_KHRONOS_validation",
         "VK_LAYER_KHRONOS_synchronization2"
     };
-    
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
-        std::cerr << "validation layer:\t\t" << pCallbackData->pMessage << std::endl;
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL
+    debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                  VkDebugUtilsMessageTypeFlagsEXT messageType,
+                  const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                  void* pUserData) {
+        std::cerr << "validation layer:\t\t" << pCallbackData->pMessage
+                  << std::endl;
         return false;
     }
 
@@ -21,13 +25,14 @@ namespace vk {
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        std::vector<const char*> extensions(
+          glfwExtensions, glfwExtensions + glfwExtensionCount);
 
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
         return extensions;
     }
-    
+
     vk_context* vk_context::s_instance = nullptr;
     vk_context::vk_context(const std::string& p_tag) {
         console_log_info("vk_context::vk_context begin initialization!!!");
@@ -39,7 +44,7 @@ namespace vk {
             .engineVersion = 1,
             .apiVersion = VK_API_VERSION_1_3,
         };
-    
+
         VkInstanceCreateInfo create_info = {
             .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
             .pNext = nullptr,
@@ -49,51 +54,57 @@ namespace vk {
 
         //! @note Setting up the required extensions for vulkan
         std::vector<const char*> extensions = get_required_extensions();
-        create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        create_info.enabledExtensionCount =
+          static_cast<uint32_t>(extensions.size());
         create_info.ppEnabledExtensionNames = extensions.data();
 
-        
         // printing out available validation layers
         uint32_t layer_count;
         std::vector<VkLayerProperties> available_validation_layers;
         vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
 
         available_validation_layers.resize(layer_count);
-        vkEnumerateInstanceLayerProperties(&layer_count, available_validation_layers.data());
+        vkEnumerateInstanceLayerProperties(&layer_count,
+                                           available_validation_layers.data());
 
         console_log_trace("================================================");
         console_log_trace("\tValidation Layers Available");
         console_log_trace("================================================");
-        for(VkLayerProperties properties : available_validation_layers) {
+        for (VkLayerProperties properties : available_validation_layers) {
             console_log_trace("Validation Layer:\t\t{}", properties.layerName);
             console_log_trace("Description\t\t{}", properties.description);
             console_log_trace("Version\t\t\t{}", (int)properties.specVersion);
         }
         console_log_trace("================================================");
 
-
-
-
         // by default we enable validation layers used for debugging!
-        create_info.enabledLayerCount = static_cast<uint32_t>(validation_layers.size());
+        create_info.enabledLayerCount =
+          static_cast<uint32_t>(validation_layers.size());
         create_info.ppEnabledLayerNames = validation_layers.data();
-    
+
         VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = {
             .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-            .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
-            .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+            .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+            .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
             .pfnUserCallback = debugCallback,
         };
-    
-        create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
-    
-        vk::vk_check(vkCreateInstance(&create_info, nullptr, &m_instance), "vkCreateInstance", __FUNCTION__);
+
+        create_info.pNext =
+          (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+
+        vk::vk_check(vkCreateInstance(&create_info, nullptr, &m_instance),
+                     "vkCreateInstance",
+                     __FUNCTION__);
 
         s_instance = this;
         console_log_info("vk_context::vk_context end initialization!!!\n\n");
     }
 
-    vk_context::~vk_context(){
+    vk_context::~vk_context() {
         // vkDestroyInstance(m_instance, nullptr);
     }
 
